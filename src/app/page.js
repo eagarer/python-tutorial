606b828d8fffebe6b979dd456f62e331ac773051
eagarer <erayagarer@gmail.com>
2025-11-12 12:51:10 +0300

    feat: add redirect

--- src/app/page.js
+++ src/app/page.js
@@ -1,30 +1,5 @@
-"use client";
-import React, { useEffect, useState } from "react";
-import { Light as SyntaxHighlighter } from "react-syntax-highlighter";
-import python from "react-syntax-highlighter/dist/esm/languages/hljs/python";
-import docco from "react-syntax-highlighter/dist/esm/styles/hljs/docco";
-
-// import { tutorial } from "@/data/tutorials.json";
-// import { useParams } from "next/navigation";
-
-// SyntaxHighlighter.registerLanguage("python", python);
+import { redirect } from "next/navigation";
 
 export default function Home() {
-  // const params = useParams();
-  // console.log("Params:", params);
-  // const [selectedTopic, setSelectedTopic] = useState(null)
-
-  // useEffect(() => {
-  //   fetch("/data/tutorials.json")
-  //     .then((res) => res.json())
-  //     .then((data) => {
-  //       setTopics(data.topics);
-  //       setActiveTopic(data.topics[0]);
-  //     });
-  // }, []);
-  return (
-    <div>
-      deneme
-      </div>
-  );
+  redirect("/intro");
 }
